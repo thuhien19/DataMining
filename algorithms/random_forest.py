bc5ef24139916d55a2bdfa4e1c44831edf0af66e
thuhien19 <167674165+thuhien19@users.noreply.github.com>
2026-05-09 15:12:21 +0700

    random forest

--- algorithms/random_forest.py
+++ algorithms/random_forest.py
@@ -1,12 +1,84 @@
+
+
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+    confusion_matrix
+)
+
+import random
 
 
-def run_random_forest(X_train, y_train):
+def run_random_forest(
+    X_train,
+    X_test,
+    y_train,
+    y_test,
+):
+
+    # ==========================================
+    # RANDOM THAM SỐ
+    # ==========================================
+
+    n_estimators = 100 
+    max_depth = 10
+
+    # ==========================================
+    # MODEL
+    # ==========================================
+
     model = RandomForestClassifier(
-        n_estimators=100,
+        n_estimators=n_estimators,
+        max_depth=max_depth,
+        class_weight="balanced",
         random_state=42
     )
 
-    model.fit(X_train, y_train)
+    # ==========================================
+    # TRAIN
+    # ==========================================
 
-    return model
+    model.fit(
+        X_train,
+        y_train
+    )
+
+    # ==========================================
+    # PREDICT
+    # ==========================================
+
+    y_pred = model.predict(
+        X_test
+    )
+
+    # ==========================================
+    # EVALUATION
+    # ==========================================
+
+    accuracy = accuracy_score(
+        y_test,
+        y_pred
+    )
+
+    report = classification_report(
+        y_test,
+        y_pred,
+        zero_division=0
+    )
+
+    matrix = confusion_matrix(
+        y_test,
+        y_pred
+    )
+
+    return (
+        model,
+        y_pred,
+        accuracy,
+        report,
+        matrix,
+        n_estimators,
+        max_depth
+    )
+
